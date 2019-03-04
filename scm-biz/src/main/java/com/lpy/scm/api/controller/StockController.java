c50e5f4b932b23c6a3205e005728fef577752bee
504697121@qq.com <lpy@lvtudiandian.com>
2019-03-04 18:59:41 +0800

    增加库存管理

--- scm-biz/src/main/java/com/lpy/scm/api/controller/StockController.java
+++ scm-biz/src/main/java/com/lpy/scm/api/controller/StockController.java
@@ -1,11 +1,20 @@
 package com.lpy.scm.api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author lpy
  * @date 2019/3/4 18:57
  */
 @Controller
+@RequestMapping("stock")
 public class StockController {
+
+    @RequestMapping("{code}")
+    @ResponseBody
+    public void queryProductByCode(@PathVariable(value = "code") String productCode, Integer num, Long price) {
+    }
 }
