b39e8abbbfdb3bb0f9897a954990bbb9be9216f3
504697121@qq.com <lpy@lvtudiandian.com>
2019-03-05 20:42:12 +0800

    增加编码查询商品

--- scm-core/src/main/java/com/lpy/scm/service/impl/StockServiceImpl.java
+++ scm-core/src/main/java/com/lpy/scm/service/impl/StockServiceImpl.java
@@ -1,6 +1,11 @@
 package com.lpy.scm.service.impl;
 
+import com.lpy.scm.dao.ProductMapper;
+import com.lpy.scm.dataobject.ProductDO;
+import com.lpy.scm.dto.ProductDTO;
 import com.lpy.scm.service.StockService;
+import com.lpy.scm.utils.BeanUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,4 +14,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StockServiceImpl implements StockService {
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Override
+    public ProductDTO queryProductByCode(String productCode) {
+        ProductDO productDO = new ProductDO();
+        productDO.setCode(productCode);
+        return BeanUtil.convertObject(productMapper.selectOne(productDO), ProductDTO.class);
+    }
 }
