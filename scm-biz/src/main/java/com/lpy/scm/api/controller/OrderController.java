5e8f4a09064c7a971e8536e3f9a09d8fb88f2dad
504697121@qq.com <lpy@lvtudiandian.com>
2019-04-02 16:55:20 +0800

    前端下单逻辑

--- scm-biz/src/main/java/com/lpy/scm/api/controller/OrderController.java
+++ scm-biz/src/main/java/com/lpy/scm/api/controller/OrderController.java
@@ -3,11 +3,13 @@ package com.lpy.scm.api.controller;
 import com.lpy.scm.bean.ApiResponse;
 import com.lpy.scm.dto.ProductDTO;
 import com.lpy.scm.exception.ParamException;
+import com.lpy.scm.param.OrderParam;
 import com.lpy.scm.service.SaleOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -24,6 +26,13 @@ public class OrderController {
     @ResponseBody
     public ApiResponse<ProductDTO> queryProductByCode(@PathVariable(value = "code") String code) throws ParamException {
         return ApiResponse.success().setData(saleOrderService.queryProductByCode(code));
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    @ResponseBody
+    public ApiResponse queryProductByCode(OrderParam orderParam) throws ParamException {
+        saleOrderService.order(orderParam);
+        return ApiResponse.success().setMsg("下单成功");
     }
 
 }
