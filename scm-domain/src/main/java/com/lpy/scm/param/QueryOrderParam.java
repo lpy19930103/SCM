f35203c68d6d4de5bba18091b04520817cfa43b6
504697121@qq.com <lpy@lvtudiandian.com>
2019-04-04 15:35:36 +0800

    增加查询订单列表前端

---
+++ scm-domain/src/main/java/com/lpy/scm/param/QueryOrderParam.java
@@ -0,0 +1,55 @@
+package com.lpy.scm.param;
+
+import java.util.Date;
+
+/**
+ * @author lpy
+ * @date 2019/4/4 15:25
+ */
+public class QueryOrderParam {
+    private Date startTime;
+    private Date endTime;
+    private String keyWord;
+    private Integer pageSize;
+    private Integer pageNo;
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+}
